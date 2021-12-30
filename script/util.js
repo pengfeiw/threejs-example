43e0dfe2370c39a2362af43e2ef26d37f0ddefd5
wangpengfei <2601339907@qq.com>
2021-12-30 16:09:18 +0800

    add photo-sphere.html

---
+++ script/util.js
@@ -0,0 +1,8 @@
+export const setSize = (container, camera, renderer) => {
+    camera.aspect = container.clientWidth / container.clientHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(container.clientWidth, container.clientHeight);
+
+    renderer.setPixelRatio(window.devicePixelRatio);
+};
